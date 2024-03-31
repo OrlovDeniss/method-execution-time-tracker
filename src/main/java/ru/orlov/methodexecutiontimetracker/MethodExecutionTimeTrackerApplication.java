81bedf9efc6fe7db2cecca13dbd2bc4b0c22173a
Orlov Denis <orlovdenis.m@yandex.ru>
2024-03-31 07:31:40 +0300

    init

---
+++ src/main/java/ru/orlov/methodexecutiontimetracker/MethodExecutionTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.orlov.methodexecutiontimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodExecutionTimeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MethodExecutionTimeTrackerApplication.class, args);
+    }
+
+}
